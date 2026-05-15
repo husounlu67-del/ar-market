3fb3333a17c181c82345976fbc38adb08aecbfc5
husounlu67-del <husounlu67@gmail.com>
2026-05-16 01:54:12 +0300

    Update ar_alarm.py

--- ar_alarm.py
+++ ar_alarm.py
@@ -3,7 +3,10 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>AR Pazar Alarm</title>
+<title>AR Pazar Alarm v2</title>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Expires" content="0">
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;600;700&family=Share+Tech+Mono&family=Exo+2:wght@300;400;600;700&display=swap');
 :root{
