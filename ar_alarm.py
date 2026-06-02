bee82920f25f4038d46fc85554af13659032333c
husounlu67-del <husounlu67@gmail.com>
2026-06-02 23:38:53 +0300

    Alarm guncellendi v20260602203852

--- ar_alarm.py
+++ ar_alarm.py
@@ -1,7 +1,7 @@
 """
 AR MARKET - PAZAR ALARM SISTEMI (Termux / Telefon)
 =====================================================
-Versiyon : 20260602203837
+Versiyon : 20260602203852
 Calistir : python ar_alarm.py
 Durdur   : Ctrl+C
 
@@ -16,7 +16,7 @@ from datetime import datetime
 # =============================================
 #  AYARLAR
 # =============================================
-VERSION          = "20260602203837"
+VERSION          = "20260602203852"
 GITHUB_RAW_URL   = "https://raw.githubusercontent.com/husounlu67-del/ar-market/main/ar_alarm.py"
 SCRIPT_PATH      = os.path.abspath(__file__)
 PCAP_PATH        = "/data/local/tmp/ar_alarm_scan.pcap"
