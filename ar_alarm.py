240715618f4f08e2529f13c57384b0adacb8b4fa
husounlu67-del <husounlu67@gmail.com>
2026-06-04 08:21:55 +0300

    Alarm guncellendi v20260604052154

--- ar_alarm.py
+++ ar_alarm.py
@@ -1,7 +1,7 @@
 """
 AR MARKET - PAZAR ALARM SISTEMI (Termux / Telefon)
 =====================================================
-Versiyon : 20260602203852
+Versiyon : 20260604052154
 Calistir : python ar_alarm.py
 Durdur   : Ctrl+C
 
@@ -16,7 +16,7 @@ from datetime import datetime
 # =============================================
 #  AYARLAR
 # =============================================
-VERSION          = "20260602203852"
+VERSION          = "20260604052154"
 GITHUB_RAW_URL   = "https://raw.githubusercontent.com/husounlu67-del/ar-market/main/ar_alarm.py"
 SCRIPT_PATH      = os.path.abspath(__file__)
 PCAP_PATH        = "/data/local/tmp/ar_alarm_scan.pcap"
@@ -1114,7 +1114,7 @@ ALARM_LIST = [
     {"name": "Hero Ring +3", "max_price": 220000000, "item_ids": ["5f15ad13"]},
     {"name": "Blue Drake Neck +0", "max_price": 1000000, "item_ids": ["501f1c13"]},
     {"name": "Amulet Of Evil Old", "max_price": 1000000, "item_ids": ["b3851b13"]},
-    {"name": "Amulet Of Evil +0", "max_price": 95000000, "item_ids": ["a6981a13"]},
+    {"name": "Amulet Of Evil +0", "max_price": 30000000, "item_ids": ["a6981a13"]},
     {"name": "Amulet Of Evil +1", "max_price": 220000000, "item_ids": ["7b991a13"]},
     {"name": "Amulet Of Evil +2", "max_price": 220000000, "item_ids": ["7c991a13"]},
     {"name": "Amulet Of Evil +3", "max_price": 220000000, "item_ids": ["7d991a13"]},
