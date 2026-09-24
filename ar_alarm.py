10db55278a0f10268c83fc9ba3ee18b8e89e6d46
husounlu67-del <husounlu67@gmail.com>
2026-09-24 22:16:02 +0300

    Alarm guncellendi v20260924191559

--- ar_alarm.py
+++ ar_alarm.py
@@ -1,7 +1,7 @@
 """
 AR MARKET - PAZAR ALARM SISTEMI (Termux / Telefon)
 =====================================================
-Versiyon : 20260915053953
+Versiyon : 20260924191559
 Calistir : python ar_alarm.py
 Durdur   : Ctrl+C
 
@@ -16,7 +16,7 @@ from datetime import datetime
 # =============================================
 #  AYARLAR
 # =============================================
-VERSION          = "20260915053953"
+VERSION          = "20260924191559"
 GITHUB_RAW_URL   = "https://raw.githubusercontent.com/husounlu67-del/ar-market/main/ar_alarm.py"
 SCRIPT_PATH      = os.path.abspath(__file__)
 PCAP_PATH        = "/data/local/tmp/ar_alarm_scan.pcap"
@@ -2359,7 +2359,7 @@ ALARM_LIST = [
     {"name": "Gold Rod +29", "max_price": 5000000000, "item_ids": []},
     {"name": "Gold Rod +30", "max_price": 5000000000, "item_ids": []},
     {"name": "Party Monster Stone +1", "max_price": 10000000, "item_ids": ["f8b8e630"]},
-    {"name": "Silver Token +1", "max_price": 100000, "item_ids": ["58a9e630"]},
+    {"name": "Silver Token +1", "max_price": 50000, "item_ids": ["58a9e630"]},
     {"name": "Gold Token +1", "max_price": 1000000, "item_ids": ["40ade630"]},
     {"name": "Rune Box 1", "max_price": 8000000, "item_ids": ["28b1e630"]},
     {"name": "STR Character Rune +1", "max_price": 50000000, "item_ids": ["2155361d"]},
@@ -2402,6 +2402,7 @@ ALARM_LIST = [
     {"name": "Max Def Character Rune +8", "max_price": 5000000000, "item_ids": ["da57361d"]},
     {"name": "Max Def Character Rune +9", "max_price": 5000000000, "item_ids": ["db57361d"]},
     {"name": "Max Def Character Rune +10", "max_price": 5000000000, "item_ids": ["dc57361d"]},
+    {"name": "Max Hp Character Rune +1", "max_price": 10000000, "item_ids": ["9757361d"]},
     {"name": "Max Hp Character Rune +2", "max_price": 20000000, "item_ids": ["9857361d"]},
     {"name": "Max Hp Character Rune +3", "max_price": 50000000, "item_ids": ["9957361d"]},
     {"name": "Max Hp Character Rune +4", "max_price": 200000000, "item_ids": ["9a57361d"]},
